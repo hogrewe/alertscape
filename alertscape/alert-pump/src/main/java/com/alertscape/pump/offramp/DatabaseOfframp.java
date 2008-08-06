564bd35768280600fc9bc8158200a46db443f4f2
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-08-06 03:00:12 +0000

    Merge from master
    git-svn-id: https://as.hogrewe.com:8888/svn/as/trunk@61 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

---
+++ alertscape/alert-pump/src/main/java/com/alertscape/pump/offramp/DatabaseOfframp.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.alertscape.pump.offramp;
+
+import com.alertscape.common.model.Event;
+
+/**
+ * @author josh
+ *
+ */
+public class DatabaseOfframp implements AlertOfframp {
+
+	public void processAlert(Event alert) {
+		
+	}
+}
---
+++ alertscape/as-common/src/main/java/com/alertscape/common/dao/AlertDao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.alertscape.common.dao;
+
+import java.util.List;
+
+import com.alertscape.common.model.Event;
+
+/**
+ * @author josh
+ *
+ */
+public interface AlertDao {
+	public void save(Event alert) throws DaoException;
+	public Event get(long alertId) throws DaoException;
+	public List<Event> getAllAlerts() throws DaoException;
+}
