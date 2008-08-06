f205fae603ccc684d1053280e16395c2cee3f04f
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-08-06 03:00:12 +0000

    Merge from master
    git-svn-id: file:///storage/as-dev/svn/as/trunk@61 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

--- alertscape/alert-pump/src/main/java/com/alertscape/pump/ConfigurableAlertPump.java
+++ alertscape/alert-pump/src/main/java/com/alertscape/pump/ConfigurableAlertPump.java
@@ -5,15 +5,31 @@ package com.alertscape.pump;
 
 import com.alertscape.AlertscapeException;
 import com.alertscape.common.model.Event;
+import com.alertscape.common.model.EventCollection;
 
 /**
  * @author josh
  *
  */
 public class ConfigurableAlertPump implements AlertPump {
+	EventCollection collection;
 	
 	public void processAlert(Event e) throws AlertscapeException {
 		
+	}
+
+	/**
+	 * @return the collection
+	 */
+	public EventCollection getCollection() {
+		return collection;
+	}
+
+	/**
+	 * @param collection the collection to set
+	 */
+	public void setCollection(EventCollection collection) {
+		this.collection = collection;
 	}
 
 }
---
+++ alertscape/as-common/src/main/java/com/alertscape/common/dao/DaoException.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.alertscape.common.dao;
+
+import com.alertscape.AlertscapeException;
+
+/**
+ * @author josh
+ *
+ */
+public class DaoException extends AlertscapeException {
+	private static final long serialVersionUID = -8583412058357312121L;
+
+	/**
+	 * 
+	 */
+	public DaoException() {
+	}
+
+	/**
+	 * @param message
+	 */
+	public DaoException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DaoException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DaoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
