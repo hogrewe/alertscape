07b047ab8ecb0e62c3455acf963276778ccc1c2e
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-08-06 03:15:04 +0000

    Squashed commit of the following:
    commit e744427d9c142c59a641bb3ff7dcec3864845ce7
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Aug 5 22:12:18 2008 -0600
    
        Add some "offramps" for the DB and jms to send to the client.
    
        Internet is down, so I can't get the activemq jars to give a real implementation.
    
    commit f2460dd5e8024a905edabe4525bc13598c20b940
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Aug 5 21:44:14 2008 -0600
    
        Add interfaces for offramps and the alert dao
    
    commit 867a346a8135c128bc3f17ae9183670333770a15
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sun Aug 3 15:35:20 2008 -0600
    
        Change interface method to processAlert
    
    commit 26ff09a6f57a85c996a8d93f35a7aa578173278b
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:29:07 2008 -0600
    
        Moved the package for the EventCollection classes
    
    commit a3bd8a3266615c3e4908c44588f3e9ef044aa044
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:28:14 2008 -0600
    
        Move the EventCollection and supporting classes to the as-common module
    
    commit 8ed40626fde841e36c80ff5c063aa4c53e8a37aa
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:25:41 2008 -0600
    
        Add the alert-pump module and fix some stuff in cev to get rid of warnings.
    
    commit 7267801f280bfcb7341789eec79416d7687ea37a
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:53:32 2008 -0600
    
        Now add in the .project and .classpath
    
    commit ab73fc67a8fdf26eb9556a7bdffee2ca631c2da4
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:52:33 2008 -0600
    
        Change the .gitignore to let eclipse .classpath and .project files in, then add in the
        .classpath and .project files for the existing projects.
    
    commit ce3bcfee42c3c1b3ab34f5a89d3ae1e195350223
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:46:23 2008 -0600
    
        Make the maven build work by getting rid of emtpy test case.  Also tuned down the performance test
        so it didn't take 30 seconds.
    
    commit 28e8bd9d6479cb4b2cc4e69cbc634dee605c6186
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:45:24 2008 -0600
    
        Now add severity
    
    commit 21a5f4aea1b05b6ef327875cf29cb77a953ba600
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:44:43 2008 -0600
    
        Move the packages for Event and Severity
    
    commit f38e17ea496cd88bbbbc1eca0701bc3b0c1f861f
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:42:04 2008 -0600
    
        moved some of the core model stuff to the as-common module
    
    commit bf9c550dce5bd6de7c7f9c1db01ebdc5bf0ac0d1
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:35:31 2008 -0600
    
        Moved logger class
    
    commit ce89fb52cc296ab303678ff5526fd69a86d48401
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:14:25 2008 -0600
    
        Add as-common again
    
    commit 6c673f699975af051cc3718dc9b96ebfece6fce1
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:12:43 2008 -0600
    
        Added as-common module
    
    commit 967f2d77f92ba220130be9ebd0683d3123e65533
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Jul 29 22:13:21 2008 -0600
    
        Remove old dot files
    
    
    git-svn-id: file:///storage/as-dev/svn/as/trunk@62 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

--- alertscape/alert-pump/src/main/java/com/alertscape/pump/offramp/JmsOfframp.java
+++ alertscape/alert-pump/src/main/java/com/alertscape/pump/offramp/JmsOfframp.java
@@ -10,6 +10,11 @@ import com.alertscape.common.model.Event;
  * @author josh
  *
  */
-public interface AlertOfframp {
-	public void processAlert(Event alert) throws AlertscapeException;
+public class JmsOfframp implements AlertOfframp {
+	
+	public void processAlert(Event alert) throws AlertscapeException {
+		// TODO Auto-generated method stub
+
+	}
+
 }
