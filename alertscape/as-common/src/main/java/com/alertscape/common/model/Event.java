06c8c466e50d90a7780bfab415ebb90c226d11bf
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-08-10 14:12:27 +0000

    Squashed commit of the following:
    commit 2f487dccb3f169262c6125d9911183c4c8904b59
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sun Aug 10 09:09:52 2008 -0600
    
        A few changes to the Event class.  Adding major and minor tags and changing the alert source to a string
        from a long.
    
    commit e744427d9c142c59a641bb3ff7dcec3864845ce7
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Aug 5 22:12:18 2008 -0600
    
        Add some "offramps" for the DB and jms to send to the client.
    
        Internet is down, so I can't get the activemq jars to give a real implementation.
    
    commit f2460dd5e8024a905edabe4525bc13598c20b940
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Aug 5 21:44:14 2008 -0600
    
        Add interfaces for offramps and the alert dao
    
    commit 867a346a8135c128bc3f17ae9183670333770a15
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sun Aug 3 15:35:20 2008 -0600
    
        Change interface method to processAlert
    
    commit 26ff09a6f57a85c996a8d93f35a7aa578173278b
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:29:07 2008 -0600
    
        Moved the package for the EventCollection classes
    
    commit a3bd8a3266615c3e4908c44588f3e9ef044aa044
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:28:14 2008 -0600
    
        Move the EventCollection and supporting classes to the as-common module
    
    commit 8ed40626fde841e36c80ff5c063aa4c53e8a37aa
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 22:25:41 2008 -0600
    
        Add the alert-pump module and fix some stuff in cev to get rid of warnings.
    
    commit 7267801f280bfcb7341789eec79416d7687ea37a
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:53:32 2008 -0600
    
        Now add in the .project and .classpath
    
    commit ab73fc67a8fdf26eb9556a7bdffee2ca631c2da4
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:52:33 2008 -0600
    
        Change the .gitignore to let eclipse .classpath and .project files in, then add in the
        .classpath and .project files for the existing projects.
    
    commit ce3bcfee42c3c1b3ab34f5a89d3ae1e195350223
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 20:46:23 2008 -0600
    
        Make the maven build work by getting rid of emtpy test case.  Also tuned down the performance test
        so it didn't take 30 seconds.
    
    commit 28e8bd9d6479cb4b2cc4e69cbc634dee605c6186
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:45:24 2008 -0600
    
        Now add severity
    
    commit 21a5f4aea1b05b6ef327875cf29cb77a953ba600
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:44:43 2008 -0600
    
        Move the packages for Event and Severity
    
    commit f38e17ea496cd88bbbbc1eca0701bc3b0c1f861f
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:42:04 2008 -0600
    
        moved some of the core model stuff to the as-common module
    
    commit bf9c550dce5bd6de7c7f9c1db01ebdc5bf0ac0d1
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:35:31 2008 -0600
    
        Moved logger class
    
    commit ce89fb52cc296ab303678ff5526fd69a86d48401
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:14:25 2008 -0600
    
        Add as-common again
    
    commit 6c673f699975af051cc3718dc9b96ebfece6fce1
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Sat Aug 2 15:12:43 2008 -0600
    
        Added as-common module
    
    commit 967f2d77f92ba220130be9ebd0683d3123e65533
    Author: Joshua Hogrewe <josh@hogrewe.com>
    Date:   Tue Jul 29 22:13:21 2008 -0600
    
        Remove old dot files
    
    
    git-svn-id: https://as.hogrewe.com:8888/svn/as/trunk@63 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

--- alertscape/as-common/src/main/java/com/alertscape/common/model/Event.java
+++ alertscape/as-common/src/main/java/com/alertscape/common/model/Event.java
@@ -4,6 +4,8 @@
 package com.alertscape.common.model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.alertscape.common.model.severity.Severity;
 
@@ -11,184 +13,205 @@ import com.alertscape.common.model.severity.Severity;
  * @author josh
  * @version $Version: $
  */
-public class Event
-{
-  public enum EventStatus
-  {
-    STANDING,
-    CLEARED;
-  }
+public class Event {
+	public enum EventStatus {
+		STANDING, CLEARED;
+	}
 
-  private long eventId;
-  private EventStatus status;
-  private String shortDescription;
-  private String longDescription;
-  private String type;
-  private Severity severity;
-  private long count;
-  private long sourceId;
-  private String item;
-  private String itemManager;
-  private String itemType;
-  private String itemManagerType;
-  private Date firstOccurence;
-  private Date lastOccurence;
+	private long eventId;
+	private EventStatus status;
+	private String shortDescription;
+	private String longDescription;
+	private String type;
+	private Severity severity;
+	private long count;
+	private String sourceId;
+	private String item;
+	private String itemManager;
+	private String itemType;
+	private String itemManagerType;
+	private Date firstOccurence;
+	private Date lastOccurence;
 
-  public long getCount( )
-  {
-    return count;
-  }
+	private Map<String, Object> majorTags;
+	private Map<String, Object> minorTags;
 
-  public void setCount(long count)
-  {
-    this.count = count;
-  }
+	public long getCount() {
+		return count;
+	}
 
-  public Date getFirstOccurence( )
-  {
-    return firstOccurence;
-  }
+	public void setCount(long count) {
+		this.count = count;
+	}
 
-  public void setFirstOccurence(Date firstOccurence)
-  {
-    this.firstOccurence = firstOccurence;
-  }
+	public Date getFirstOccurence() {
+		return firstOccurence;
+	}
 
-  public String getItem( )
-  {
-    return item;
-  }
+	public void setFirstOccurence(Date firstOccurence) {
+		this.firstOccurence = firstOccurence;
+	}
 
-  public void setItem(String item)
-  {
-    this.item = item;
-  }
+	public String getItem() {
+		return item;
+	}
 
-  public String getItemManager( )
-  {
-    return itemManager;
-  }
+	public void setItem(String item) {
+		this.item = item;
+	}
 
-  public void setItemManager(String itemManager)
-  {
-    this.itemManager = itemManager;
-  }
+	public String getItemManager() {
+		return itemManager;
+	}
 
-  public String getItemManagerType( )
-  {
-    return itemManagerType;
-  }
+	public void setItemManager(String itemManager) {
+		this.itemManager = itemManager;
+	}
 
-  public void setItemManagerType(String itemManagerType)
-  {
-    this.itemManagerType = itemManagerType;
-  }
+	public String getItemManagerType() {
+		return itemManagerType;
+	}
 
-  public String getItemType( )
-  {
-    return itemType;
-  }
+	public void setItemManagerType(String itemManagerType) {
+		this.itemManagerType = itemManagerType;
+	}
 
-  public void setItemType(String itemType)
-  {
-    this.itemType = itemType;
-  }
+	public String getItemType() {
+		return itemType;
+	}
 
-  public Date getLastOccurence( )
-  {
-    return lastOccurence;
-  }
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
 
-  public void setLastOccurence(Date lastOccurence)
-  {
-    this.lastOccurence = lastOccurence;
-  }
+	public Date getLastOccurence() {
+		return lastOccurence;
+	}
 
-  public Severity getSeverity( )
-  {
-    return severity;
-  }
+	public void setLastOccurence(Date lastOccurence) {
+		this.lastOccurence = lastOccurence;
+	}
 
-  public void setSeverity(Severity severity)
-  {
-    this.severity = severity;
-  }
+	public Severity getSeverity() {
+		return severity;
+	}
 
-  public long getSourceId( )
-  {
-    return sourceId;
-  }
+	public void setSeverity(Severity severity) {
+		this.severity = severity;
+	}
 
-  public void setSourceId(long sourceId)
-  {
-    this.sourceId = sourceId;
-  }
+	public String getSourceId() {
+		return sourceId;
+	}
 
-  public String getType( )
-  {
-    return type;
-  }
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
 
-  public void setType(String type)
-  {
-    this.type = type;
-  }
+	public String getType() {
+		return type;
+	}
 
-  public boolean isStanding( )
-  {
-    return getStatus( ) == EventStatus.STANDING;
-  }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-  public long getEventId( )
-  {
-    return eventId;
-  }
+	public boolean isStanding() {
+		return getStatus() == EventStatus.STANDING;
+	}
 
-  public void setEventId(long eventId)
-  {
-    this.eventId = eventId;
-  }
+	public long getEventId() {
+		return eventId;
+	}
 
-  public String getLongDescription( )
-  {
-    return longDescription;
-  }
+	public void setEventId(long eventId) {
+		this.eventId = eventId;
+	}
 
-  public void setLongDescription(String longDescription)
-  {
-    this.longDescription = longDescription;
-  }
+	public String getLongDescription() {
+		return longDescription;
+	}
 
-  public String getShortDescription( )
-  {
-    return shortDescription;
-  }
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
 
-  public void setShortDescription(String shortDescription)
-  {
-    this.shortDescription = shortDescription;
-  }
+	public String getShortDescription() {
+		return shortDescription;
+	}
 
-  public EventStatus getStatus( )
-  {
-    return status;
-  }
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
 
-  public void setStatus(EventStatus status)
-  {
-    this.status = status;
-  }
+	public EventStatus getStatus() {
+		return status;
+	}
 
-  @Override
-  public boolean equals(Object o)
-  {
-    return getEventId( ) == ((Event) o).getEventId( );
-  }
+	public void setStatus(EventStatus status) {
+		this.status = status;
+	}
 
-  @Override
-  public int hashCode( )
-  {
-    return Long.valueOf(getEventId( )).hashCode( );
-  }
+	/**
+	 * @return the majorTags
+	 */
+	public Map<String, Object> getMajorTags() {
+		return majorTags;
+	}
+
+	/**
+	 * @param majorTags
+	 *            the majorTags to set
+	 */
+	public void setMajorTags(Map<String, Object> majorTags) {
+		this.majorTags = majorTags;
+		if(this.majorTags == null) {
+			this.majorTags = new HashMap<String, Object>();
+		}
+	}
+	
+	public void addMajorTag(String name, Object value) {
+		majorTags.put(name, value);
+	}
+	
+	public Object getMajorTag(String name) {
+		return majorTags.get(name);
+	}
+
+	/**
+	 * @return the minorTags
+	 */
+	public Map<String, Object> getMinorTags() {
+		return minorTags;
+	}
+
+	/**
+	 * @param minorTags
+	 *            the minorTags to set
+	 */
+	public void setMinorTags(Map<String, Object> minorTags) {
+		this.minorTags = minorTags;
+		if(this.minorTags == null) {
+			this.minorTags = new HashMap<String, Object>();
+		}
+	}
+	
+	public void addMinorTag(String name, Object value) {
+		minorTags.put(name, value);
+	}
+	
+	public Object getMinorTag(String name) {
+		return minorTags.get(name);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return getEventId() == ((Event) o).getEventId();
+	}
+
+	@Override
+	public int hashCode() {
+		return Long.valueOf(getEventId()).hashCode();
+	}
 
 }
