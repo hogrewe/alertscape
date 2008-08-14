dd83813dc9995c640315f052ac328b5d05bdd28a
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-08-14 03:39:01 +0000

    Merge from master
    git-svn-id: file:///storage/as-dev/svn/as/trunk@65 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

--- alertscape/alert-pump/src/main/java/com/alertscape/pump/ConfigurableAlertPump.java
+++ alertscape/alert-pump/src/main/java/com/alertscape/pump/ConfigurableAlertPump.java
@@ -10,7 +10,6 @@ import java.util.Set;
 
 import com.alertscape.AlertscapeException;
 import com.alertscape.common.model.Alert;
-import com.alertscape.common.model.AlertSource;
 import com.alertscape.pump.offramp.DatabaseOfframp;
 import com.alertscape.pump.offramp.JmsOfframp;
 
@@ -21,16 +20,8 @@ import com.alertscape.pump.offramp.JmsOfframp;
 public class ConfigurableAlertPump implements AlertPump {
 	private DatabaseOfframp dbOfframp;
 	private JmsOfframp jmsOfframp;
-	private Set<AlertSource> sources = new HashSet<AlertSource>();
-	private Map<AlertSource, Set<Alert>> sourceToAlertsMap = new HashMap<AlertSource, Set<Alert>>();
 
 	public void processAlert(Alert a) throws AlertscapeException {
-		AlertSource source = a.getSource();
-		Set<Alert> alerts = sourceToAlertsMap.get(source);
-		if(alerts == null) {
-			alerts = new HashSet<Alert>();
-			sourceToAlertsMap.put(source, alerts);
-		}
 	}
 
 	/**
@@ -46,7 +37,6 @@ public class ConfigurableAlertPump implements AlertPump {
 	 */
 	public void setDbOfframp(DatabaseOfframp dbOfframp) {
 		this.dbOfframp = dbOfframp;
-	}
 
 	/**
 	 * @return the jmsOfframp
