dbeeee6b19b1cbc8ef8bbad04d4017702890e91a
josh <josh@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2008-09-21 01:20:46 +0000

    Changed generator
    git-svn-id: file:///storage/as-dev/svn/as/trunk@90 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

---
+++ alertscape/cev/src/main/java/com/alertscape/cev/ui/swing/RandomTransportAlertGenerator.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.alertscape.cev.ui.swing;
+
+import java.util.Random;
+
+import com.alertscape.common.model.Alert;
+
+/**
+ * @author josh
+ * 
+ */
+public class RandomTransportAlertGenerator {
+  private static final String[] bandwidths = { "OC48", "OC12", "OC3", "T3", "T1", "DS3", "DS1" };
+  private static final String[] cities = { "DENCO", "LAXCA", "CHIIL", "NYCNY", "SEAWA", "MIAFL" };
+  private static final String[] devices = { "15454", "DMX", "OPT", "INF" };
+  private static final String[] types = { "Threshold Crossing", "LOS", "LOF" };
+
+  private Random rand = new Random();
+
+  public Alert readAlert() {
+    Alert a = new Alert();
+
+    String device = devices[rand.nextInt(devices.length)];
+    String city = cities[rand.nextInt(cities.length)];
+    String bandwidth = bandwidths[rand.nextInt(bandwidths.length)];
+    String port = bandwidth + "-" + (rand.nextInt(3)+1) + "-" + (rand.nextInt(3)+1) + "-"
+        + (rand.nextInt(24)+1);
+    String type = types[rand.nextInt(types.length)];
+    
+    String itemManager = city + device + "00" + (rand.nextInt(9)+1);
+
+    a.setType(type);
+    a.setItem(itemManager + "-"+ port);
+    a.setItemType(device + "-" + bandwidth);
+    a.setItemManager(itemManager);
+    a.setItemManagerType(device);
+    a.setLongDescription(itemManager + "-" + port + " " + type);
+
+    return a;
+  }
+}
