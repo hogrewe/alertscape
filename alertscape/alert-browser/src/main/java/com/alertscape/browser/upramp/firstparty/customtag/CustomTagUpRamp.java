b53269af65c795ee17faae6cffc2cbeec21c6c59
snusz <snusz@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2009-01-03 19:44:27 +0000

    git-svn-id: https://as.hogrewe.com:8888/svn/as/trunk@116 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

--- alertscape/alert-browser/src/main/java/com/alertscape/browser/upramp/firstparty/customtag/CustomTagUpRamp.java
+++ alertscape/alert-browser/src/main/java/com/alertscape/browser/upramp/firstparty/customtag/CustomTagUpRamp.java
@@ -2,11 +2,13 @@ package com.alertscape.browser.upramp.firstparty.customtag;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import com.alertscape.browser.model.BrowserContext;
 import com.alertscape.browser.upramp.model.UpRamp;
+import com.alertscape.common.model.Alert;
 
 public class CustomTagUpRamp implements UpRamp
 {
@@ -32,7 +34,21 @@ public class CustomTagUpRamp implements UpRamp
 
 	public boolean submit(BrowserContext context, Map keyvals)
 	{
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub - this is just hacked for now, it adds the tags locally and returns,
+		// it needs to send the alerts and data up to the server, and have this same thing happen on the server, like an onramp
+		// so that all clients get the same change, not just this one
+		List<Alert> alerts = context.getSelectedAlerts();
+		Iterator<Alert> it = alerts.iterator();
+		
+		String tagName = (String)keyvals.get(CustomTagConstants.TAG_NAME);
+		String tagValue = (String)keyvals.get(CustomTagConstants.TAG_VALUE);
+		
+		while (it.hasNext())
+		{
+			Alert alert = it.next();
+			alert.addMinorTag(tagName, tagValue);
+		}
+		
 		return true;
 	}
 
