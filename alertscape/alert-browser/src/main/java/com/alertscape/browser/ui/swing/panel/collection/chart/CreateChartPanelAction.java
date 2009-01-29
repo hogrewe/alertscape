2c96d9306fc3e8ad8b852e01ae30da3f453f634c
snusz <snusz@2e23e1bd-1a53-0410-a7f7-f1ace52beed6>
2009-01-29 18:26:08 +0000

    git-svn-id: https://as.hogrewe.com:8888/svn/as/trunk@142 2e23e1bd-1a53-0410-a7f7-f1ace52beed6

---
+++ alertscape/alert-browser/src/main/java/com/alertscape/browser/ui/swing/panel/collection/chart/CreateChartPanelAction.java
@@ -0,0 +1,103 @@
+package com.alertscape.browser.ui.swing.panel.collection.chart;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+import com.alertscape.browser.ui.swing.AlertBrowser;
+import com.alertscape.common.model.Alert;
+
+public class CreateChartPanelAction extends AbstractAction
+{
+	private static final long serialVersionUID = 1L;
+	private JFrame parentFrame;
+	private ImageIcon myIcon;
+
+	public CreateChartPanelAction()
+	{	
+		super();
+		putValue(NAME, getActionTitle());
+		putValue(SMALL_ICON, getActionIcon());
+		putValue(SHORT_DESCRIPTION, getActionDescription());
+		putValue(MNEMONIC_KEY, getActionMnemonic());
+		putValue(ACCELERATOR_KEY, getActionAccelerator());		
+	}
+	
+	public void setParentFrame(JFrame parentFrame)
+	{
+		this.parentFrame = parentFrame;
+	}
+
+	public void actionPerformed(ActionEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		Thread t = new Thread() 
+		{
+			public void run()
+			{
+				// create a new pie chart panel with the alerts that are selected currently in the table
+				List<Alert> alerts = AlertBrowser.getCurrentContext().getSelectedAlerts();
+				if (alerts.size() > 0)
+				{
+					String title = "Pie Chart (item," + alerts.size() + ")";
+					String tooltip = title;
+					AlertPieChartPanel piechart = new AlertPieChartPanel(alerts, "item", title, tooltip);
+											
+					// add the chart to a tab, adjacent to the tabular view
+					AlertBrowser.addTabbedPanel(title, null, piechart, tooltip);
+				}
+			}
+		};
+		t.start();		
+	}	
+	
+
+	protected KeyStroke getActionAccelerator()
+	{
+		return KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK);
+	}
+
+
+	protected String getActionDescription()
+	{
+		return "Chart Selected Alerts";
+	}
+
+
+	protected Icon getActionIcon()
+	{
+		if (myIcon == null)
+		{
+			URL imageUrl = getClass().getResource("/com/alertscape/images/mini/icon_component.gif");
+		    ImageIcon icon = new ImageIcon(imageUrl);
+		    myIcon = icon;	
+		}
+		
+		return myIcon;
+	}
+
+
+	protected int getActionMnemonic()
+	{
+		return KeyEvent.VK_H;
+	}
+
+
+	protected String getActionTitle()
+	{
+		return "Chart Selected Alerts";
+	}
+
+
+	protected String getActionWindowTitle()
+	{
+		return "Chart Selected Alerts";
+	}
+}
